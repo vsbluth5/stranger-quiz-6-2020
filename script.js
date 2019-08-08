1046328f1e2d184f32b71341b67f6e3e92ab60d2
Glitch (hello-webpage) <none>
2019-08-08 13:30:08 +0000

    🐛🚡 Checkpoint ./script.js:574715/181 ./style.css:574715/1374

--- script.js
+++ script.js
@@ -4,11 +4,3 @@
 // prints "hi" in the browser's dev tools console
 console.log('hi');
 
-let inputBox = document.querySelector('input')
-
-console.log(inputBox)
-
-inputBox.addEventListener("input", (e) => {
-  console.log(inputBox.value)
-  inputBox.value = ""
-})
