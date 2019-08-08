095a7c9a55041e2732752bd2746e0bf309537349
Glitch (hello-webpage) <none>
2019-08-08 14:54:01 +0000

    🙍🚎 Checkpoint ./script.js:574715/431

--- script.js
+++ script.js
@@ -14,6 +14,10 @@ userInput.addEventListener("input", (e) => {
   console.log(userInput.value)
   if (userInput.value == "Eleven") {
     console.log("YOU GOT IT!")
-    pictureBox.innerHTML 
+    pictureBox.innerHTML += `<img src="https://cdn.glitch.com/01b27d37-a07e-4e2e-8caf-659256274937%2FScreen%20Shot%202019-08-08%20at%208.39.11%20AM.png?v=1565269664594"/>`
+  } else if (userInput.value == "Dustin") {
+    pictureBox.innerHTML += `<img src = "https://cdn.glitch.com/01b27d37-a07e-4e2e-8caf-659256274937%2FScreen%20Shot%202019-08-08%20at%208.39.21%20AM.png?v=1565269664037"/>`
+  } else if (userInput.value == "Mike") {
+    
   }
 })
