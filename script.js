6159283b7b0d5efedbe2179ea751e94a5459d37f
Glitch (hello-webpage) <none>
2019-08-20 19:13:05 +0000

    🙅👆 Checkpoint ./script.js:574715/712 ./index.html:574715/150

--- script.js
+++ script.js
@@ -12,4 +12,9 @@ console.log(pictureBox)
 
 textbox.addEventListener("input", (e)=> {
   console.log("Key pressed!")
+  console.log(textbox.value)
+  if (textbox.value === "Eleven"){
+    console.log("YOU WIN!")
+    pictureBox.innerHTML += '<img src="https://cdn.glitch.com/01b27d37-a07e-4e2e-8caf-659256274937%2FScreen%20Shot%202019-08-08%20at%208.39.11%20AM.png?v=1565269664594"/>'
+  }
 })
