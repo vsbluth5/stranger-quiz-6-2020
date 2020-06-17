ffc878b910fb118fb893565c8d386f666738fd19
Glitch (hello-webpage) <none>
2020-06-17 01:24:58 +0000

    🐷💿 Checkpoint ./script.js:3856783/794

--- script.js
+++ script.js
@@ -4,5 +4,20 @@
 // prints "hi" in the browser's dev tools console
 console.log('Script Running');
 
+// Select the input element using its id, and store that in a variable called textbox
+const textbox = document.querySelector("#userInput")
+
+// Log that element to the console just to confirm the query selector worked.
+console.log(textbox)
+
+// textbox and #userInput refer to the same thing. The code in this file
+// refers to that html element as textbox from now on.
+
+// Challenge: There is one other element in the html that this file will l
+// need to refer to. What is it?
+
+const pictureBox = document.querySelector("#pictureBox")
+console.log(pictureBox)
+
 
 
